03f1868cd7155492d5aa341dd938b10fee60dac3
Samuel Murray <samuel.murray@outlook.com>
2023-12-21 19:47:51 +0100

    Day 18.2 complete

--- Sources/Day18/code.swift
+++ Sources/Day18/code.swift
@@ -4,50 +4,6 @@ enum Direction {
   case up, down, right, left
 }
 
-enum Orientation {
-  case southCorner, northCorner, vertical, horizontal
-}
-
-struct Trench {
-  var edge: [Position: Orientation]
-}
-
-extension Trench {
-  func enclosedRegion() -> Set<Position> {
-    var enclosed = Set(edge.map { $0.key })
-    let xRange = edge.map { $0.key.x }.minAndMax()!
-    let yRange = edge.map { $0.key.y }.minAndMax()!
-    for x in (xRange.min...xRange.max) {
-      for y in (yRange.min...yRange.max) {
-        let pos = Position(x, y)
-        if isInside(pos, edge, xRange.min) {
-          enclosed.insert(pos)
-        }
-      }
-    }
-    return enclosed
-  }
-}
-
-func isInside(_ position: Position, _ edge: [Position: Orientation], _ xMin: Int) -> Bool {
-  if edge.keys.contains(position) {
-    return false
-  }
-  var numIntersections = 0
-  for x in (xMin..<position.x) {
-    let pos = Position(x, position.y)
-    if let orientation = edge[pos] {
-      switch orientation {
-      case .vertical, .southCorner:
-        numIntersections += 1
-      default:
-        continue
-      }
-    }
-  }
-  return numIntersections % 2 == 1
-}
-
 struct Position: Equatable, Hashable {
   let x: Int
   let y: Int
@@ -58,76 +14,13 @@ struct Position: Equatable, Hashable {
 }
 
 extension Position {
-  var up: Position {
-    return Position(self.x, self.y - 1)
-  }
-  var down: Position {
-    return Position(self.x, self.y + 1)
-  }
-  var left: Position {
-    return Position(self.x - 1, self.y)
-  }
-  var right: Position {
-    return Position(self.x + 1, self.y)
-  }
-
-  func move(direction: Direction) -> Position {
+  func move(direction: Direction, steps: Int) -> Position {
     switch direction {
-    case .up: self.up
-    case .right: self.right
-    case .left: self.left
-    case .down: self.down
+    case .up: Position(x, y - steps)
+    case .down: Position(x, y + steps)
+    case .right: Position(x + steps, y)
+    case .left: Position(x - steps, y)
     }
-  }
-}
-
-struct DigInstruction {
-  let direction: Direction
-  let steps: Int
-  let color: String
-}
-
-func parseTrench(_ input: [String]) throws -> Trench {
-  var position = Position(0, 0)
-  var edgePieces = [position]
-  for line in input {
-    let parts = line.split(separator: " ")
-    let direction = try parseDirection(parts[0].first!)
-    let steps = Int(parts[1])!
-    for _ in 0..<steps {
-      position = position.move(direction: direction)
-      edgePieces.append(position)
-    }
-  }
-
-  var edge: [Position: Orientation] = [:]
-  for i in edgePieces.dropFirst().dropLast().indices {
-    let pos = edgePieces[i]
-    let previous = edgePieces[i - 1]
-    let next = edgePieces[i + 1]
-    edge[pos] = try determineOrientation(previous: previous, current: pos, next: next)
-  }
-  edge[edgePieces.first!] = try determineOrientation(
-    previous: edgePieces.last!, current: edgePieces.first!, next: edgePieces[1])
-  edge[edgePieces.last!] = try determineOrientation(
-    previous: edgePieces[edgePieces.count - 2], current: edgePieces.last!, next: edgePieces.first!)
-
-  return Trench(edge: edge)
-}
-
-func determineOrientation(previous: Position, current: Position, next: Position) throws
-  -> Orientation
-{
-  if previous.y == current.y && next.y == current.y {
-    return .horizontal
-  } else if previous.x == current.x && next.x == current.x {
-    return .vertical
-  } else if previous.y > current.y || next.y > current.y {
-    return .southCorner
-  } else if previous.y < current.y || next.y < current.y {
-    return .northCorner
-  } else {
-    throw AocError.runtimeError("Bad input")
   }
 }
 
@@ -141,7 +34,99 @@ func parseDirection(_ input: Character) throws -> Direction {
   }
 }
 
+func parseDirectionHex(_ input: Character) throws -> Direction {
+  return switch input {
+  case "0": .right
+  case "1": .down
+  case "2": .left
+  case "3": .up
+  default: throw AocError.runtimeError("Bad input \(input)")
+  }
+}
+
+func computeTurn(newDirection: Direction, previousDirection: Direction) -> Turn {
+  return switch newDirection {
+  case .up: previousDirection == .right ? .left : .right
+  case .down: previousDirection == .right ? .right : .left
+  case .right: previousDirection == .up ? .right : .left
+  case .left: previousDirection == .up ? .left : .right
+  }
+}
+
+enum Turn {
+  case left, right
+}
+
+struct Polygon {
+  let corners: [Position]
+  let turns: [Turn]
+}
+
+extension Polygon {
+  func area() -> Int {
+    var area = 0
+    var j = corners.count - 1
+    for i in 0..<corners.count {
+      area += (corners[j].x + corners[i].x) * (corners[j].y - corners[i].y)
+      area += max(abs(corners[j].x - corners[i].x) - 1, 0)
+      area += max(abs(corners[j].y - corners[i].y) - 1, 0)
+      j = i
+    }
+    let turnArea = turns.map {$0 == .left ? 1 : 3}.reduce(0, +) / 4
+    return abs(area / 2) + turnArea
+  }
+}
+
 func computeArea(_ input: [String]) throws -> Int {
-  let trench = try parseTrench(input)
-  return trench.enclosedRegion().count
+  let polygon = try parsePolygon(input)
+  return polygon.area()
+}
+
+func computeAreaHex(_ input: [String]) throws -> Int {
+  let polygon = try parsePolygonHex(input)
+  return polygon.area()
+}
+
+
+func parsePolygonHex(_ input: [String]) throws -> Polygon {
+  var position = Position(0, 0)
+  var corners: [Position] = []
+  var turns: [Turn] = []
+
+  let parts = input.last!.split(separator: " ")
+  var lastDirection = try parseDirectionHex(parts[2].dropLast().last!)
+
+  for line in input {
+    let parts = line.split(separator: " ")
+    let hex = parts[2].dropFirst().dropFirst().dropLast()
+    let direction = try parseDirectionHex(hex.last!)
+    let steps = Int(hex.dropLast(), radix: 16)!
+    position = position.move(direction: direction, steps: steps)
+    corners.append(position)
+
+    turns.append(computeTurn(newDirection: direction, previousDirection: lastDirection))
+    lastDirection = direction
+  }
+  return Polygon(corners: corners.reversed(), turns: turns)
+}
+
+func parsePolygon(_ input: [String]) throws -> Polygon {
+  var position = Position(0, 0)
+  var corners: [Position] = []
+  var turns: [Turn] = []
+
+  let parts = input.last!.split(separator: " ")
+  var lastDirection = try parseDirection(parts[0].first!)
+
+  for line in input {
+    let parts = line.split(separator: " ")
+    let direction = try parseDirection(parts[0].first!)
+    let steps = Int(parts[1])!
+    position = position.move(direction: direction, steps: steps)
+    corners.append(position)
+
+    turns.append(computeTurn(newDirection: direction, previousDirection: lastDirection))
+    lastDirection = direction
+  }
+  return Polygon(corners: corners.reversed(), turns: turns)
 }
--- Tests/Day18Tests/test.swift
+++ Tests/Day18Tests/test.swift
@@ -4,26 +4,30 @@ import XCTest
 @testable import Day18
 
 class Day18Tests: XCTestCase {
-  func testTrenchEdgeSize() throws {
+  func testShoelace1() throws {
     let input = [
-      "R 6 (#70c710)",
-      "D 5 (#0dc571)",
-      "L 2 (#5713f0)",
-      "D 2 (#d2c081)",
-      "R 2 (#59c680)",
-      "D 2 (#411b91)",
-      "L 5 (#8ceee2)",
-      "U 2 (#caa173)",
-      "L 1 (#1b58a2)",
-      "U 2 (#caa171)",
-      "R 2 (#7807d2)",
-      "U 3 (#a77fa3)",
-      "L 2 (#015232)",
-      "U 2 (#7a21e3)",
+      "R 2",
+      "D 1",
+      "L 2",
+      "U 1",
     ]
-    let trench = try parseTrench(input)
+    let result = try computeArea(input)
 
-    XCTAssertEqual(38, trench.edge.count)
+    XCTAssertEqual(6, result)
+  }
+
+  func testShoelace2() throws {
+    let input = [
+      "R 1",
+      "U 1",
+      "R 1",
+      "D 2",
+      "L 2",
+      "U 1",
+    ]
+    let result = try computeArea(input)
+
+    XCTAssertEqual(8, result)
   }
 
   func testGivenInput1() throws {
@@ -45,6 +49,28 @@ class Day18Tests: XCTestCase {
 
     let result = try computeArea(input)
 
-    print(result)  // 62500
+    print(result)
+  }
+
+  func testGivenInput2() throws {
+    guard let filePath = Bundle.module.url(forResource: "test-input1", withExtension: "txt") else {
+      throw AocError.runtimeError("File not found")
+    }
+    let input = try readFile(filePath)
+
+    let result = try computeAreaHex(input)
+
+    XCTAssertEqual(952408144115, result)
+  }
+
+  func testUnknownInput2() throws {
+    guard let filePath = Bundle.module.url(forResource: "input1", withExtension: "txt") else {
+      throw AocError.runtimeError("File not found")
+    }
+    let input = try readFile(filePath)
+
+    let result = try computeAreaHex(input)
+
+    print(result)
   }
 }
